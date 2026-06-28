d11820bac9e2d10fe8080e2ef73a058ee804eb70
Tucker Max <maxmackpdx@gmail.com>
2026-06-28 16:48:11 -0700

    Shift Spotted wallpaper crop left to hide Pride Ride text

--- client/src/lib/spottedHero.ts
+++ client/src/lib/spottedHero.ts
@@ -8,7 +8,7 @@ export const spottedHeroContent = {
   tagline: "Not sure what missed connections are? Grab your nearest daddy.",
   taglineAccent: "magenta",
   bgImage: "/motifs/hero-spotted-bigfoot.jpg",
-  bgPosition: "center 42%",
+  bgPosition: "18% 40%",
 } as const satisfies Partial<PageHeroProps>;
 
 export function spottedHeroProps(overrides: Partial<PageHeroProps> = {}): PageHeroProps {
