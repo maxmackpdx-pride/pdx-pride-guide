9cf60c452e51570ed93177514a217eadbc359300
Tucker Max <maxmackpdx@gmail.com>
2026-09-08 16:36:18 -0700

    Add Barklow mountain and Barlow campground motifs

---
+++ script/outz-motifs/draw-barklow-barlow.py
@@ -0,0 +1,14 @@
+from drawing import pine,save
+bark='https://wanderingyuncks.com/2018/09/20/barklow-mountain-and-bullards-beach/'
+save({
+'usfs-5233010526':('Barklow summit landscape with Copper Mountain view, manzanita and old lookout-site stones; no standing tower is depicted.',bark,'https://farm2.staticflickr.com/1890/43820782945_5892a7e90e_b.jpg',
+'<path d="M17 155L64 129 92 145 144 108 194 146 245 130M17 177Q91 159 141 179T245 171M51 236L78 206 113 201 137 217 168 203 220 233M94 225l15-9 18 11-12 9ZM153 222l13-11 16 5-5 15ZM188 247V224M188 237Q171 233 175 221Q187 215 194 227Q202 211 214 223Q218 238 201 239"/>'+pine(33,205,.65)),
+'usfs-11231010526':('Barklow northern mountain approach represented by its shared ridge and collapsed shelter timbers amid fir forest; reference is connected mountain trail.',bark,'https://farm2.staticflickr.com/1865/29793101697_18615fa77c_b.jpg',
+'<path d="M20 158L80 115 122 143 174 111 241 141M69 205L114 170 163 208M81 198L135 182M92 209L156 190M78 218H172M121 170L133 212M163 185L145 218M19 247Q85 225 137 240T245 226M76 230Q95 219 84 205"/>'+pine(33,222,1.5)+pine(227,217,1.3)+pine(186,183,.65)),
+'usfs-11232010526':('Barklow west approach: twisting madrone and tanoak canopy, pine edge and climbing forest path.',bark,'https://farm2.staticflickr.com/1867/30859336368_f0ece17b05_b.jpg',
+'<path d="M40 223Q64 190 56 161Q47 141 63 119M55 178L29 159M54 163L88 144M33 148Q14 141 24 122Q26 108 43 114Q48 95 68 109Q85 97 92 113Q115 114 108 134Q99 151 80 148M93 246Q140 213 117 192T147 154M111 249Q160 214 137 192T160 156M172 225l14-9 20 6-7 13ZM64 243l14-12 12 8-5 10Z"/>'+pine(220,221,1.5)+pine(185,171,.7)),
+'usfs-5090010324':('Barlow Creek forest campground: small creek beside a shaded tent clearing and the historic Barlow Road corridor.','https://thedyrt.com/camping/oregon/oregon-barlow-creek-campground','https://thedyrt.com/camping/oregon/oregon-barlow-creek-campground',
+'<path d="M18 185Q87 165 135 198T246 210M19 208Q87 186 135 221T243 235M81 157L121 94 162 157ZM121 94V157L141 126M86 170H167M171 150H218M181 150L175 170M208 150L219 170M58 195H78M154 221H173M63 248Q74 225 95 219M83 250Q95 235 109 232"/>'+pine(37,176,1.7)+pine(229,143,1.45)),
+'usfs-5081010324':('Barlow Crossing creekside camp: water winding below a sloped bank, scattered stones and shaded camping.','https://thedyrt.com/camping/oregon/oregon-barlow-crossing-campground','https://thedyrt.com/camping/oregon/oregon-barlow-crossing-campground/gallery',
+'<path d="M16 131Q77 114 139 136T245 139M88 141Q134 157 111 181T144 216Q177 233 227 243M68 143Q112 161 89 182T123 236M24 231L52 184 84 231ZM52 184V231M22 244H96M181 180l14-11 17 12-10 9ZM147 201l13-8 11 8-4 9ZM170 114H219M178 114L172 133M208 114L220 133"/>'+pine(37,134,1.45)+pine(231,213,1.15)),
+})
