a4c4b7beb830ac6693b8f9a4d22f56e60dbf2bae
Tucker Max <maxmackpdx@gmail.com>
2026-09-08 16:37:19 -0700

    Add Battle Ax and Bear Camp landscape motifs

---
+++ script/outz-motifs/draw-battle-bearcamp.py
@@ -0,0 +1,14 @@
+from drawing import pine,save
+brandy='https://adventuresoflupe.com/?tag=brandy-peak'
+save({
+'usfs-6433007010602':('Battle Ax summit rock and old lookout footings above Elk Lake, with Mount Jefferson on the horizon.','https://cherylhill.net/blog/category/bull-of-the-woods-wilderness/','https://www.flickr.com/photos/tags/oldcascades/',
+'<path d="M17 145L60 128 91 143 143 93 171 129 246 142M129 112L142 120 149 106M31 174Q80 156 117 168Q137 181 104 188Q56 191 31 174ZM112 245L135 206 165 194 183 218 229 242M147 221h17v10h-17ZM177 222h16v10h-16ZM123 241L150 237M189 242L211 237"/>'+pine(32,222,.7)+pine(79,229,.65)),
+'usfs-5326.005181':('Bear Camp Ridge FSR23 approach: timbered ridge and saddle trail toward Brandy Peak, based on connected Bear Camp Ridge route photographs.',brandy,brandy,
+'<path d="M18 155L72 121 103 143 150 119 206 143 244 129M22 186Q94 157 135 177T246 168M90 246Q54 216 122 205T174 177M112 249Q80 223 142 216T192 182M177 241l16-14 15 13-7 9ZM29 233l14-9 10 12Z"/>'+pine(36,207,1.25)+pine(220,201,1.1)+pine(183,166,.5)),
+'usfs-10109010526':('Brandy Peak narrow rocky summit ridge with successive Coast Range hills visible below it; connected trail photo reference.',brandy,brandy,
+'<path d="M18 155L65 135 106 149 154 127 207 150 245 132M19 180L69 160 109 176 154 158 246 177M62 246L88 207 120 198 146 167 168 192 189 205 218 245M88 207L115 222 120 198M146 167L151 208 168 192M151 208L172 230 189 205M103 244L115 222M177 245L172 230"/>'+pine(30,224,.6)),
+'usfs-10110010526':('Bearcamp High Ridge: forested crest with switchbacks on the south slope and a glimpse of the bald ridge above Brandy Peak approach.',brandy,brandy,
+'<path d="M19 168L65 147 111 119 155 132 203 113 246 150M53 245Q139 230 108 207Q64 191 116 181T187 159M76 249Q166 233 130 207Q99 191 138 190T204 164M175 221L202 199 238 216M182 231L200 218 225 229"/>'+pine(33,208,1.1)+pine(223,190,.85)+pine(79,160,.55)),
+'usfs-5076010324':('Bear Springs campground group picnic shelter and tent clearing among east-side Mount Hood pines.','https://www.recreation.gov/camping/campgrounds/234720','https://www.recreation.gov/camping/campgrounds/234720#photos',
+'<path d="M64 105L130 64 195 105ZM76 106V171M185 106V171M103 140H159M109 151H166M117 140L109 168M149 140L158 168M71 177H190M39 231L74 180 108 231ZM74 180V231M137 216Q181 202 242 221M19 245Q111 232 245 248"/>'+pine(32,183,1.8)+pine(229,181,1.7)),
+})
