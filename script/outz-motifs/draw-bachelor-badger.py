3e8411d411be60b918c08c918cf9ec4454c24d60
Tucker Max <maxmackpdx@gmail.com>
2026-09-08 16:33:19 -0700

    Add Bachelor Badger and Bagby custom landscape motifs

---
+++ script/outz-motifs/draw-bachelor-badger.py
@@ -0,0 +1,13 @@
+from drawing import pine,save
+lake='https://commons.wikimedia.org/wiki/File:Panoramic_of_Badger_Lake-Mt_Hood_(23346354642).jpg'
+photo='https://upload.wikimedia.org/wikipedia/commons/d/d1/Panoramic_of_Badger_Lake-Mt_Hood_%2823346354642%29.jpg'
+save({
+'usfs-6432979010602':('Bachelor Mountain near Detroit: meadow switchbacks and a rocky alcove framing Coffin Mountain; distinct from Mount Bachelor ski volcano.','https://adamschneider.net/photos/2024-06-bm/?p=20240611-120247','https://adamschneider.net/photos/2024-06-bm/image/20240611-120247.jpg',
+'<path d="M23 180L38 134 67 110 84 133 90 175M22 181L47 147 67 130 77 174M86 155L125 130 157 134 194 161 242 148M16 215Q83 188 127 205T245 190M90 247Q78 224 149 222Q213 219 177 204Q144 193 164 181M111 247Q105 233 173 231Q241 225 198 204L178 190M41 238V218M36 219L41 211 47 219 41 225ZM210 247V228M205 228L210 220 217 228 210 234"/>'+pine(226,183,.6)),
+'usfs-5993.004901':('Lower Badger Creek at the dry east-side forest transition: oak canopy, pines, rocky canyon and winding creek.','https://trailsnw.com/trails/oregon-cascades/badger-creek','https://trailsnw.com/trails/oregon-cascades/badger-creek',
+'<path d="M20 176L61 137 95 155M168 140L207 113 244 150M122 144Q74 178 122 201T130 246M139 146Q97 178 142 199T153 245M24 239Q54 201 85 191M36 245Q67 214 91 204M53 166V122M53 134Q23 136 27 116Q20 99 41 100Q47 87 63 98Q84 90 85 112Q97 130 72 139M53 149L71 130M176 213l17-13 17 12-8 11ZM194 171l11-8 12 10-17 7Z"/>'+pine(230,220,1.25)),
+'usfs-5051010324':('Badger Lake campground: forested bowl lake framed by a tent clearing and driftwood shoreline; USFS panoramic photo. ',lake,photo,
+'<path d="M18 99L66 67 101 79 144 54 198 77 246 68M31 134Q97 111 182 121Q253 132 225 152Q185 171 113 163M67 141H180M111 152H203M24 239L64 174 107 239ZM64 174V239L85 207M132 219L224 189M137 229L229 199M158 211L167 223M193 200L202 210M100 249Q174 238 245 247"/>'+pine(30,177,1.1)+pine(229,169,1.3)),
+'usfs-6015.004901':('Badger Lake trail approach: path descending around a forested slope to the lake, with the shared trailhead sign. ',lake,photo,
+'<path d="M18 158L63 129 93 139 137 111 192 132 244 117M82 169Q139 151 205 166Q250 181 222 197Q184 214 105 200M118 181H207M140 193H215M19 244Q94 239 80 218Q35 194 76 179M31 251Q112 245 97 215Q62 195 89 184M173 241l14-9 14 7-6 10Z"/>'+pine(34,205,1.1)+pine(226,166,.7)+pine(191,164,.55)),
+})
---
+++ script/outz-motifs/draw-bagby.py
@@ -0,0 +1,7 @@
+from drawing import pine,save
+save({
+'usfs-4290630010602':('Bagby campground forest tent clearing at the creek confluence, distinct from the hot-spring bathhouse farther up the trail.','https://thedyrt.com/camping/oregon/bagby-campground','https://thedyrt.com/camping/oregon/bagby-campground/gallery',
+'<path d="M19 190Q82 160 128 196T244 233M19 214Q83 184 126 220T241 251M244 164Q174 169 157 199M245 187Q198 186 178 210M85 161L123 100 166 161ZM123 100V161L143 128M91 173H173M180 146H224M189 146L180 166M213 146L223 166M50 212H67M190 236H211"/>'+pine(34,167,1.8)+pine(227,146,1.6)),
+'usfs-5147010324':('Bagby hot-springs trail: timber bathhouse roof and carved log soaking tub, steam lines, and forest footbridge.','https://wanderingyuncks.com/2018/07/16/bagby-trail-to-silver-king-lake/','https://www.tophotsprings.com/wp-content/uploads/2019/05/private-soaking-tubs-at-bagby-hot-springs-oregon-1600x1066.jpg',
+'<path d="M65 141L124 99 184 141M77 134V200M171 134V200M81 201H168M92 172Q125 159 159 172Q125 183 92 172ZM92 172L96 190Q126 200 155 190L159 172M113 160Q104 151 115 143M135 158Q126 148 136 138M17 224L92 214M17 238L95 228M26 223V237M48 220V234M70 217V231M106 226Q160 212 246 231M116 242Q173 228 245 247"/>'+pine(32,204,1.2)+pine(223,203,1.15)),
+})
